2046a331801857be3630ddb4755598a55ed3c306
Yuchen <yuchenzhang.study@gmail.com>
2019-05-06 10:08:28 -0600

    post code

--- code/data_analysis.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('../data/PS_20174392719_1491204439457_log.csv')
-# column names ['step', 'type', 'amount', 'nameOrig', 'oldbalanceOrg', 'newbalanceOrig',
-# 'nameDest', 'oldbalanceDest', 'newbalanceDest', 'isFraud',
-# 'isFlaggedFraud']
-print(df.head())
